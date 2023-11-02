4e3ad65f0dd4c62ad6c1f9cfc3a8886c9a0bd623
baga(Bagdaulet) <bagdaulet_seilov@epam.com>
2023-11-02 17:57:02 +0100

    add mongoDB docker compose yml,

--- src/main/java/kz/monsha/taboobot/config/YamlConfigReader.java
+++ src/main/java/kz/monsha/taboobot/config/YamlConfigReader.java
@@ -2,12 +2,13 @@ package kz.monsha.taboobot.config;
 
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
+
 import java.io.InputStream;
 import java.util.Map;
 
 public class YamlConfigReader {
 
-    private Map<String, Object> properties;
+    private final Map<String, Object> properties;
 
     public YamlConfigReader(String fileName) {
         Yaml yaml = new Yaml(new Constructor(Map.class));
@@ -20,14 +21,22 @@ public class YamlConfigReader {
             throw new RuntimeException("Failed to load YAML file", e);
         }
 
-
-
     }
 
-    public Object getValue(String key) {
-        return properties.get(key);
+    public <T> T getValue(String key) {//TODO still cannot compile ${BOT_KEY:botkey} expression
+        String[] keyParts = key.split("\\.");
+        Map<String, Object> nestedMap = properties;
+
+        for (int i = 0; i < keyParts.length - 1; i++) {
+            String part = keyParts[i];
+            if (nestedMap.containsKey(part) && nestedMap.get(part) instanceof Map) {
+                nestedMap = (Map<String, Object>) nestedMap.get(part);
+            } else {
+                return null;
+            }
+        }
+
+        return (T) nestedMap.get(keyParts[keyParts.length - 1]);
     }
 
-    // You can add more methods to navigate through the nested structures
-    // as per your requirement
 }
